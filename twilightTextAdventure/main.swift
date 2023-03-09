7e4c8965fe59e953b2b9e3b9f119bde153407b11
Witherton <katelyn.gosnell@stu.barren.kyschools.us>
2023-03-09 09:07:18 -0600

    testing it out

--- twilightTextAdventure/main.swift
+++ twilightTextAdventure/main.swift
@@ -2,10 +2,80 @@
 //  main.swift
 //  twilightTextAdventure
 //
-//  Created by Katelyn Gosnell on 2/6/23.
+//  Created by Witherton on 2/6/23.
 //
 
-import Foundation
+let start = "WELCOME TO FORKS, WASHINGTON! IT'S TIME TO START YOUR FIRST DAY OF SCHOOL! Please press return to start the game."
 
-print("Hello, World!")
+let enter = "Which do you go with? Press 1 or 2 to continue."
 
+let gameOver = "Sorry GAME OVER!"
+
+let newLine = "\n"
+
+let q1 = "You arrive to Forks, Wasington. A small town of under 5,000 residents. Your father Charlie, the towns sherrif, has just showed you your old childhood room. The papers hung on the wall haven't been touched since you last left 3 years ago. You sit your tiny catus in the window seal. Your dad tells you he hopes you enjoy and leaves you be. The best thing about Charlie. He doesn't hover. A little while later, you hear a truck pull up outside. You go donw stairs to see who it is. It's your old friend Jacob Black and his father. They greet you with open arms. Your dad announces that the truck they brought over is your welcome gift. You are excited. Jacob mentiones how he rebuilt the engine himself. You now have a new truck to help you get to school." + newLine + "The next day, you arrive at school. March. Middle of the semester. Great. You walk into the halls and look for your classes using the sheet you recived from the front office when you checked in. You are greated by two students. One is a kind young boy. He begins to ramble about how he is head of the news paper and how he will be your buddy for as long as you need. The other is a young girl. She eyes you up and down before smiling at you like she is has something up your sleeve. She peeks at your paper and flashes you a fake smile. 'I can help you get to your class.' The young boy butts in. 'So can I. We have some of the same classes.'" + newLine + "Go with the young boy" + newLine + "OR" + newLine + "2. Go with the young girl." + newLine + enter
+
+let gameOver1 = "You go with the young girl. She leads you to the wrong class and you are laughed at by some of your peers. You get embarrassed and run back to the office. You decide maybe you should skip today. You sign yourself out and get in your truck to go home. On the way home, get in a wreak because your too worried about the event that happned moments ago and you don't see the deer in the road and die." + gameOver
+
+let q2 = "You go with the young boy. He is excited and mentioned his name. Eric. What a sweet name. As you walk to english, he mentioned how he wants to put you in the news paper. You politly decline. He is a bit dissapointed." + newLine + "English goes well and he helpes you to gymnastics. There, after cahnging, you are told you are to play volly ball. You are frantic. Oh no! But you are so clumbsy and uncordinated!! How ever will you play! but like crule fate has it, they ignore your pleas to be on the side line. During a game, you accidently hit the volly ball into the back of the head of a one of the basketball players. You run over to apologize. The man smiles at you and introduces himself at Mike. The glimmer in his eye shows he likes you. Oh the hardships of being the main character in a early 2000's romance novle. After only a briefe moment of talking, you are apporched by another girl. She laughes about how uncordinated you are and introduces herself as Jessica. She tires to flirt with Mike but he is too smitten by you. After a while another girl comes up to your three and ask you for help. Jessica is persistant you let her help." + newLine + "1. You help the girl" + newLine + "OR" + newLine + "2. You let Jessica help the girl" + newLine + enter
+
+let gameOver2 = "You go to help the girl but are hit with a stray volly ball. You hit the wooden floor so hard you go into a coma. You die weeks later when your mother decided to pull the plug. Your father never forgives himself." + gameOver
+
+let q3 = "You let Angilia go help the girl and you and Mike go your seprate ways. After Gym, you go to Algebra 2. No one you have meet so far is in this class, so you sit in the back of the class quietly. Class was easy and goes by quickly. You look down at your paper and see you now go to lunch!" + newLine + "You meet Eric in the hall and he also proclaimes he has lunch next. He insist you sit with him and his friends. How sweet. Once in the lunch room and get your lunch, Eric leads you to a table. He even pulls out the chair for you. The glimmer in his eye tells you he likes you. Oh no. Not another one. You thank him and sit down. The table holds all the people you have meet today. What a conquincidenc and totaly not bad writing. All the people you have meet. All but one girl. She is talking to Eric about the school paper. She soon introduces herself as Angela. You two make comments about what the next artical in the school new paper shoudl be. You land on teen eating disorders. All the sudden, the cafeteria doors are open and a gush of stratejicaly placed wind flows through your hair. You turn and see three couples and a lownly sexy boy all by himself. Jessica takes the honers in telling you who these kids are. The Cullens. Children of Docter Cullen and his wife Esme. All the children were adopted by the two, and now have borderline incestiral relationships with eachother. Jessica says it's weird. Angele tries to defend it. You make a mental note to ask how Angela feels about her siblings if she has any. The one lone sexy boy passes you. Jessica mentions his name. Edward Cullen. She mummbles to herself that Edward thinks he's too good for anyone in this school. You don't care. You are looking at lonely sexy Edward Cullen. When he sits down. He shares a smile to his brother and then looks at you. His eyes narrow and his brow furrow. He seems confused by you. How romantic. There is not glimmer in his eye, showing he does not like you. As lunch continues, Jessica makes comments about how to not go after Edward and you keep making eye contact with Edward every 30 seconds. The other students are scared." + newLine + "After lunch, Mike and Eric walk you to Biology. You last class of the day. Mike says you should sit by him. The teacher says you should sit beside lonely sexy Edward Cullen, since there is an open seat next to him." + newLine + "1. You listen to the teacher and go sit next to lonely sexy Edward Cullen" + newLine + "OR" + newLine + "2. You go sit next to Mike. " + newLine + enter
+
+let gameOver3 = "You sit next to Mike and enjoy class. He tells you about what the class is learning about and flirts with you all class. You end your day on a good note and turn in a slip of paper to the office. You go home and live a normal and healthy life in Forks with your father. You marry and start a beautiful family. You grow old and happy and die surrounded by family that loves you. COngradulations. But. Sadly, it is not to be celebrated. Because you need to be the mindless main character who sits next to Lonely Sexy Edward Cullen." + gameOver
+
+let q4 = "You follow the teacher's word and sit next to lonely sexy Edward. Once you sit next to him, he covers his mouth and looks away from you. You begine to worry. Do you smell bad? You smell your hair and clothes. Nope. You smell like strawberries and vanillia because of &^%#ing course you smell like strawberries and %$@#ing vanillia you &^$%#@#%#&. As the teacher begins the class lession, lonely sexy Edward avoide touching you. You find it strange but ultimently decide to ignore him. This is your last mentally sane decision you make in your life. Can I get an F in the chat. You feel his eeys on you all of class. Somehting in your gut tells you to run. That he is going to kill you. You think about if maybe you should look at him and touch his hand as a form of introducti0on." + newLine + "1. You leave poor lonely sexy Edward alone." + newLine + "OR" + newLine + "2. You look poor emo edgy lonely sexy Edward in the eyes and gentely touch his poor emo edgy lonely sexy hand and introduce yourself." + newLine + enter
+
+let gameOver4 = "You touch his hand and you are meeted with the look of Death itself when you meet his eyes. He can not hold back anymore. He goes feral. He attacks you and drinks your blood right then and there. The class goes crazy. In his desprate attempts to cover up his doing, he kills the class and teacher. In your last dying moments, you are greeted with the fact you have the residents of the Biology class on your hands. You die a murderer as Edward makes his esapce to Alaska. His family move laster that week. None of the families are given a exsplanation to what happned to their sweet children. Your father dedicates the rest of his life to finding out how his beloved child died. He perishes without answers. He never forgave himself." + gameOver
+
+let q5 = "You tell yourself to ignore him yet again. Class goes by un-natrually and before the dissmissing beel even rings lonely sexy Edward is out the door. You shake your head as Mike and Eric rush over to you. Both ask how you felt about the class. You say it was fine and head to the front office to drop off the slip of paper to the front desk lady. There, you are greeted with a very upset lonely sexy Edward who is franticly trying to swich his classes. the front desk lady tells him it can't be done. He scoffes and crosses his arms. a naisly voice is hard as he glars at the lady. 'My fatehr will hear about th- oh wait no sorry. Wrong early 2000's book franchises. My apologies. Lonely sexy Edward huffs and storms out. He glars at you on his way out an dmummbles under his breath. Too low for you to make it out completly but it sounded close to him saying he will just 'have to indure it' What ever the hell that means. You hand the lady your slip of paper and she thanks you. Once you exit the office, you see Eric and Mike have waited for you and as you three walk to the parking lot, they ask you how you like the school and the classes. You nod along to their chatter till you get to your truck. You turn to tell them to have a nice day, and to thank them for all their help. They both blush and look down at the gravel. Eric even maneges out a nervouse giggle. That is when Eric invites you to come hang out at a small mall strip on the other side of town with Mike, Angele, Jessuica, Laurean, Tyler, and himself. Mike nods his head, and says you should totally come along." + newLine + "1. You are tired from the day, and decide to go home after nthanking them for the invatation." + newLine + "OR" + newLine + "2. Despite how tired you feel, you agree to go hang out with them." + newLine + enter
+
+let gameOver5 = "The two boys High Five eachother and Mike pats your shoulders. You follow their cars out of the parking lot and after 20 minuets of driving, you arrive at a small strip mall. You have a wonderful time and head home a bit after dark. Your father asked were you have been and you excitinly tell nhim how much fun you had. He is a bit upset you didn't at least call him and tell him, but he is more then thrilled you enjoyed your first day so much. You go to bed with a smile on your face and a few new shirts hanging up in your closet. But here yet again, that wasn't very clumbsy shy uncordinated main chatacter of you, so I will have to make you die in your sleep so you can choose more unwisely next time. I'll just write in you got so happy you got a heart attack or somehting weird. Idk...... Why are you still here... go back." + gameOver
+
+let win = "You politly decline and mention how tired you are. Both boys are sad but understand today was probably really draining. You get in yout truck and go home. Your father is still at work so you decide to go ahead and do your homework and start dinner for you both. Your father gets home right on time as dinner is done as well as you homework. You fill him in on your first day and how strange and tiring it was. He says he understand and after eating in comfortable silence, you are off to bed. You get a call from your mother and tell her how lonely sexy Edward treated you. She tells you to confront him. After hanging up, you make a mental note to do that Monday. And with that. You lay your self down into a nice sleep. Ingoring the cold sweat running down your back telling you you are beeing watched from soemthing in your room." + newLine + "CONGRATULATIONS! YOU SURVIED YOUR FIRST DAY AT SCHOOL!! I can't wait to see what kind of adventures await you when you return to Forks High. Home of the Trojans. See ya soon."
+
+print(start)
+
+let response = readLine()
+print(q1)
+
+var userInput = readLine()
+
+if (userInput == "1"){
+    print(q2)
+    userInput = readLine()
+    
+    if (userInput == "2"){
+        print(q3)
+        userInput = readLine()
+        
+        if (userInput == "1"){
+            print(q4)
+            userInput = readLine()
+            
+            if (userInput == "1"){
+                print(q5)
+                userInput = readLine()
+                
+                if (userInput == "1"){
+                    print(win)
+                } else {
+                    print(gameOver5)
+                }
+                
+            } else {
+                print(gameOver4)
+            }
+            
+        } else {
+            print(gameOver3)
+        }
+        
+    } else {
+        print(gameOver2)
+    }
+    
+} else {
+    print(gameOver1)
+}
